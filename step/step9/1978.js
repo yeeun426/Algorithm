079bffd9e01fd73d6494dc2ba8c9ea7cb98f236e
yeeun426 <thsudkcla7@naver.com>
2023-04-02 02:40:19 +0900

    [-ing] BOJ_9단계

---
+++ step/step9/1978.js
@@ -0,0 +1,1 @@
+let input = require('fs').readFileSync('예제.txt').toString().trim().split("\n");
--- step/step9/2501.js
+++ step/step9/2501.js
@@ -1,1 +1,25 @@
-let input = require('fs').readFileSync('예제.txt').toString().split("\n");
+let input = require('fs').readFileSync('예제.txt').toString().trim().split("\n").map(Number);
+
+for(let i = 0 ; i <= input.length ; i++) {
+    const ans = [];
+    let sum = 0;
+
+    if(input[i] === -1) break;
+
+    for(let j = 1 ; j < input[i] ; j++) {
+        if(input[i] % j === 0 && ans.indexOf(j) === -1)  {
+            ans.push(j);
+        }
+    }
+
+    for(let k = 0 ; k < ans.length ; k++) {
+        sum += ans[k];
+    }
+    
+    if(sum == input[i]) {
+        let plus = ans.join(" + ")
+        console.log(`${input[i]} = ${plus}`)
+    } else {
+        console.log(`${input[i]} is NOT perfect.`);
+    }
+}

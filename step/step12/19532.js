1972ff819e306dca44845a8e2797bd91d759aed1
yeeun426 <thsudkcla7@naver.com>
2023-04-26 01:07:35 +0900

    [-ing] BOJ_12단계

---
+++ step/step12/19532.js
@@ -0,0 +1,10 @@
+let [a, b, c, d, e, f] = require('fs').readFileSync('예제.txt').toString().trim().split(" ").map(Number);
+
+for(let x = -999 ; x <= 999 ; x++) {
+    for(let y = -999 ; y <= 999 ; y++){
+        if((a * x + b * y === c) && (d * x + e * y === f)) {
+            console.log(x, y);
+            break;
+        }
+    }
+}
--- step/step12/2231.js
+++ step/step12/2231.js
@@ -1,14 +1,12 @@
 let input = +require('fs').readFileSync('예제.txt').toString().trim();
 // 198 + 1 + 9 + 8 = 198 + 18 = 216
-const ans = 0;
+const ans = [];
+
 for(let i = 0 ; i < input-1 ; i++) {
-    const answer = i.toString().split("").reduce((acc,cur) => acc += Number(cur), 0);
-    if(answer+i === input) {
-        console.log(i);
-        break;
-    }
-    if(i === input-1) {
-        console.log(0);
-        break;
+    const sum = i.toString().split("").reduce((acc,cur) => acc += Number(cur), 0);
+    if(sum+i === input) {
+        ans.push(i);
     }
 }
+
+console.log(ans[0] ? ans[0] : 0);

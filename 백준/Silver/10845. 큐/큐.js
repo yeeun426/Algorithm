01555e2d2d6650570d90c750fe8f4c8cc0541350
yeung <88296511+yeeun426@users.noreply.github.com>
2023-05-26 00:04:32 +0900

    [Silver IV] Title: 큐, Time: 148 ms, Memory: 12520 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.js
@@ -0,0 +1,38 @@
+let [N, ...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+
+let queue = [];
+let result = [];
+
+for(let i = 0 ; i < N ; i++) {
+    const command = input[i].trim().split(" ")[0];
+    switch(command) {
+        case 'push' :
+            queue.push(+input[i].split(" ")[1]);
+            break;
+
+        case 'pop' :
+            result.push(queue.shift() || -1);
+            break;
+
+        case 'size' :
+            result.push(queue.length);
+            break;
+
+        case 'empty' :
+            result.push(queue[0] ? 0 : 1);
+            break;
+
+        case 'front' :
+            result.push(queue[0] || -1);
+            break;
+
+        case 'back' :
+            result.push(queue[queue.length-1] || -1);
+            break;
+
+        default :
+            break;
+   }
+}
+
+console.log(result.join("\n"))

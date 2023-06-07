dd462b8ace9be66739020795c0d1efb2573b7ff0
yeung <88296511+yeeun426@users.noreply.github.com>
2023-06-07 15:50:27 +0900

    [Silver II] Title: 쇠막대기, Time: 232 ms, Memory: 13188 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.js
@@ -0,0 +1,25 @@
+let pieces = require('fs').readFileSync('/dev/stdin').toString().split("");
+
+let answer = 0;
+let sticks = [];
+
+for(let i = 0 ; i < pieces.length; i++) {
+    const iron = pieces[i];
+
+    // 레이저 발사하면 쇠막대기 개수만큼 조각 생김
+    if(iron === "(" && pieces[i+1] === ")") {
+        answer += sticks.length;
+        i++;
+
+    // 쇠막대기 왼쪽 끝
+    } else if (iron === "(") {
+        sticks.push(iron);
+
+    // 쇠막대기 오른쪽 끝 (조각 마지막 하나 더 생기면서 끝남)
+    } else if (iron === ")") {
+        sticks.pop();
+        answer++;
+    }
+}
+
+console.log(answer);

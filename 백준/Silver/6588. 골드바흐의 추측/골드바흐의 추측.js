49217fbc11db55df19e9ca3c4bf826fd9391dced
yeung <88296511+yeeun426@users.noreply.github.com>
2023-06-17 17:20:45 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 512 ms, Memory: 75736 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.js
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.js
@@ -4,9 +4,10 @@ const n = 1000001;
 const table = Array(n).fill(1);
 
 // 소수가 아닌 모든 홀수 처리
-for (let i = 2 ; i <= Math.ceil(Math.sqrt(n)) ; i++) {
-    for(let j = i ** 2 ; j <= n ; j+=i) {
-        table[j] = 0;
+// 가장 작은 소수인 3부터 2칸씩 (홀수) 건너뛰며 확인
+for (let i = 3 ; i <= Math.ceil(Math.sqrt(n)) ; i+=2) {
+    if(table[i]){
+        for(let j = i ** 2 ; j <= n ; j+=i) table[j] = 0;
     }
 }
 
@@ -16,6 +17,8 @@ for(let k = 0 ; k < input.length ; k++) {
     if(!input[k]) break; // 0이면 끝내기
 
     let exist = false;
+
+    // 처음 발견되는 것이 가장 차가 큰 수들임
     for(let i = 3 ; i <= parseInt(input[k]/2) ; i+=2) {
         if(table[i] && table[input[k]-i]) {
             result.push(`${input[k]} = ${i} + ${input[k] - i}`);

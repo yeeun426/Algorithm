b34a2b22b40e5f5b67bf1095c242d8745b27fce0
yeung <88296511+yeeun426@users.noreply.github.com>
2023-06-17 17:07:43 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 536 ms, Memory: 59920 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.js
@@ -0,0 +1,29 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split("\n").map(Number);
+
+const n = 1000001;
+const table = Array(n).fill(1);
+
+// 소수가 아닌 모든 홀수 처리
+for(let i = 3 ; i < Math.ceil(Math.sqrt(n)) ; i += 2) {
+    if(table[i]) {
+        for(let j = i * 2 ; j < n ; j+=i) table[j] = 0;
+    }
+}
+
+const result = [];
+
+for(let k = 0 ; k < input.length ; k++) {
+    if(!input[k]) break; // 0이면 끝내기
+
+    let exist = false;
+    for(let i = 3 ; i <= parseInt(input[k]/2) ; i+=2) {
+        if(table[i] && table[input[k]-i]) {
+            result.push(`${input[k]} = ${i} + ${input[k] - i}`);
+            exist = true;
+            break;
+        }
+    }
+    if (!exist) result.push("Goldbach's conjecture is wrong.");
+}
+
+console.log(result.join("\n"));

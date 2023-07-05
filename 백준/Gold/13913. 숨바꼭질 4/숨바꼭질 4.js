480ae1d3717721c488cd1471773035b07f389de5
yeung <88296511+yeeun426@users.noreply.github.com>
2023-07-05 23:35:53 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 3496 ms, Memory: 25204 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.js
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.js
@@ -1,41 +1,36 @@
-const sol = (input) => {
-  const [N, K] = input.split(" ").map(Number);
-  const path = Array.from({ length: 100100 }, () => 0);
-  const visit = Array.from({ length: 100100 }, () => 0);
-  function bfs(N) {
-    const queue = [];
-    queue.push([N, 0]);
-    visit[N] = 1;
-    while (queue.length) {
-      const [cur, time] = queue.shift();
-      if (cur === K) return time;
-      for (next of [cur - 1, cur + 1, cur * 2]) {
-        if (!visit[next] && next >= 0 && next <= 100000) {
-          path[next] = cur;
-          visit[next] = 1;
-          queue.push([next, time + 1]);
-        }
-      }
-    }
-  }
+let [N, K] = require('fs').readFileSync('/dev/stdin').toString().split(" ").map(Number);
 
-  const time = bfs(N);
-  const order = [];
-  order.push(K);
-  let prev = path[K];
-  for (let i = 0; i < time; i++) {
+const visited = Array(100001).fill(0);
+const path = Array(100001).fill(0);
+
+function bfs(N) {
+    const queue = [];
+    queue.push([N, 0]); // [수빈이의 위치, 0]
+
+    while (queue.length) {
+        // cur : 현재 위치, time : 초
+        const [cur, time] = queue.shift();
+
+        //현재 위치와 동생의 위치(k)가 같을 때 break;
+        if (cur === K) return time
+
+        for (next of [cur - 1, cur + 1, cur * 2]) {
+            if (!visited[next] && next >= 0 && next <= 100000) {
+                path[next] = cur;
+                visited[next] = 1;
+                queue.push([next, time + 1]);
+            }
+        }
+    }
+}
+
+const time = bfs(N);
+const order = [];
+order.push(K);
+let prev = path[K];
+for (let i = 0; i < time; i++) {
     order.push(prev);
     prev = path[prev];
-  }
-  const result = `${time}\n${order.reverse().join(" ")}`;
-  return result;
-};
+}
 
-require("readline")
-  .createInterface(process.stdin, process.stdout)
-  .on("line", (line) => {
-    console.log(sol(line));
-  })
-  .on("close", () => {
-    process.exit();
-  });
+console.log(`${time}\n${order.reverse().join(" ")}`);

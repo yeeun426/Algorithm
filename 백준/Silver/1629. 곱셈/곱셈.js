413cc558a23921c0bcc68b1efb8bc4b3b4dc37cf
yeung <88296511+yeeun426@users.noreply.github.com>
2023-07-31 23:51:01 +0900

    [Silver I] Title: 곱셈, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const [A,B,C] = fs.readFileSync("./dev/stdin").toString().trim().split(' ').map(BigInt)
+
+function pow(a,b){
+  if(b==0){
+  return BigInt(1); 
+  }else{
+    const temp = pow(a, BigInt(parseInt(b/BigInt(2))))
+    if(b%BigInt(2)==0){
+      return  (temp * temp) % C;
+    }else{
+      return (temp * temp * a) % C;
+    }
+  }
+}
+
+
+console.log(parseInt(pow(A,B)))

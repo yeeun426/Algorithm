b8873f403c6ea7bccb8235090480120a8f2c810c
yeung <88296511+yeeun426@users.noreply.github.com>
2023-12-01 16:49:47 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,24 +1,25 @@
 function solution(board, moves) {
     var answer = 0;
-    const boardTopColumnIdxs = Array(board[0].length).fill(0)
-
-    for(let column=0;column<board[0].length;column++){
-        for(let row=0;row<board.length;row++){
+    const isExistIdx = Array(board.length).fill(0);
+    
+    for(let column = 0 ; column < board.length ; column++){
+        for(let row = 0 ; row < board.length ; row++){
             if(board[row][column] !== 0){
-                boardTopColumnIdxs[column] = row
+                isExistIdx[column] = row // 격자 안에 인형이 들어있기 시작하는 위치 idx
                 break;
             }
         }
     }
-
+    
     const stack = []
-    moves.forEach(move=>{
-        const columnIdx = move-1
-        const rowIdx = boardTopColumnIdxs[columnIdx]
-        boardTopColumnIdxs[columnIdx]++
+    moves.forEach(move => {
+        const columnIdx = move-1;
+        const rowIdx = isExistIdx[columnIdx];
+        isExistIdx[columnIdx]++;
+        
         if(board[rowIdx] !== undefined){
-            const lastStackValue = stack.at(-1)
-            const pushStackValue = board[rowIdx][columnIdx]
+            const lastStackValue = stack.at(-1);
+            const pushStackValue = board[rowIdx][columnIdx];
             if(lastStackValue === pushStackValue){
                 stack.pop()
                 answer += 2

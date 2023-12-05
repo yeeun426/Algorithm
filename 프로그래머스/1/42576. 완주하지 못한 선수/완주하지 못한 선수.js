60d0d346f9099ad86f99f13455a07635ae81ca30
yeung <88296511+yeeun426@users.noreply.github.com>
2023-12-05 18:37:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.81 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,14 +1,11 @@
 function solution(participant, completion) {
     var answer = '';
 
-    participant.sort();
-    completion.sort();
     
-    for(let i = 0; i < participant.length; i++) {
-        if(participant[i] !== completion[i]) {
-            answer += participant[i];
-            break;
-        }
-    } 
+    for(let i = 0 ; i < completion.length ; i++) {
+        let idx = participant.indexOf(completion[i]);
+        participant.splice(idx, 1);
+    }
+    answer += participant[0]
     return answer;
 }

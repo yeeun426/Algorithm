0c884f99ee64c178eee2aa8d9e5cc7108fd8faac
yeung <88296511+yeeun426@users.noreply.github.com>
2023-12-20 17:21:52 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,25 +1,22 @@
 function solution(progresses, speeds) {
     var answer = [];
-    const remainigArr = [];
+    const remainingArr = [];
     for(let i = 0 ; i < progresses.length ; i++) {
         remaining = 100-progresses[i];
-        remainigArr.push(Math.ceil(remaining/speeds[i]));
+        remainingArr.push(Math.ceil(remaining/speeds[i]));
     }
-    let maxDay = remainigArr[0];
-    let deployCount = 1;
-
-    for (let i = 1; i < remainigArr.length; i++) {
-        if (remainigArr[i] <= maxDay) {
-            // 현재 기능이 이전 배포에 함께 배포될 것
-            deployCount++;
+    
+    let maxDay = remainingArr[0];
+    let deployCnt = 1;
+    for (let i = 1; i < remainingArr.length; i++) {
+        if (maxDay >= remainingArr[i] ) {
+            deployCnt++;
         } else {
-            // 현재 기능부터 새로운 배포 시작
-            answer.push(deployCount);
-            deployCount = 1;
-            maxDay = remainigArr[i];
+            answer.push(deployCnt);
+            deployCnt = 1;
+            maxDay = remainingArr[i];
         }
     }
-    // 마지막 배포 처리
-    answer.push(deployCount);
+    answer.push(deployCnt);
     return answer;
 }

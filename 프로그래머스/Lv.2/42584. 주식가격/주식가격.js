a754b4d12305eb76cb0ab289df7fd52e3171aa74
yeung <88296511+yeeun426@users.noreply.github.com>
2024-01-12 17:32:39 +0900

    [level Lv.2] Title: 주식가격, Time: 5.17 ms, Memory: 42.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42584. 주식가격/주식가격.js
@@ -0,0 +1,15 @@
+function solution(prices) {
+    var answer = [];
+    
+    for (let i = 0; i < prices.length; i++) {
+        let sec = 0;
+        for (let j = i + 1; j < prices.length; j++) {
+            sec++;
+            if (prices[i] > prices[j]) {
+                break;
+            }
+        }
+        answer.push(sec);
+    }
+    return answer;
+}

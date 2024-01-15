5be48e7311686162d283f0759142d2debb237ce0
yeung <88296511+yeeun426@users.noreply.github.com>
2024-01-15 19:49:20 +0900

    [level Lv.3] Title: 디스크 컨트롤러, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/Lv.3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,35 +1,23 @@
 function solution(jobs) {
-    const n = jobs.length;
-
-    let answer = 0;
-    let currentTime = 0; // 현재 시간
-
-    while (jobs.length > 0) {
-        let minIndex = -1;
-        let minDuration = Number.MAX_SAFE_INTEGER;
-
-        for (let i = 0; i < jobs.length; i++) {
-            const [start, duration] = jobs[i];
-
-            if (start <= currentTime && duration < minDuration) {
-                // 작업의 시작 시간이 현재 시간 이전이고 소요시간이 가장 적은 작업 선택
-                minIndex = i;
-                minDuration = duration;
-            }
+    var answer = 0;
+    jobs.sort((a, b) => a[0] - b[0]);
+    const queue = [];
+    let time = 0;
+    let i = 0;
+    while(queue.length > 0 || i < jobs.length) {
+        if(jobs.length > i && jobs[i][0] <= time) {
+            queue.push(jobs[i++]);
+            queue.sort((a, b) => a[1] - b[1]);
+            continue;
         }
-
-        if (minIndex === -1) {
-            // 현재 시간 이전에 들어온 작업이 없으면 현재 시간을 증가시킴
-            currentTime++;
-        } else {
-            // 선택한 작업을 수행하고 배열에서 제거
-            const [start, duration] = jobs[minIndex];
-            currentTime += duration;
-            answer += currentTime - start;
-            jobs.splice(minIndex, 1);
+        if(queue.length > 0) {
+            let [ start, work ] = queue.shift();
+            answer = answer + time - start + work;
+            time += work;
+        } else if(queue.length == 0) {
+            time = jobs[i][0];
         }
     }
 
-    // 평균 계산 후 소수점 이하 버림
-    return Math.floor(answer / n);
+    return parseInt(answer / jobs.length);
 }

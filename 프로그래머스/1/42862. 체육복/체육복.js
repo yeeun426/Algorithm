ad358a7a6c7338c17855983223688919e3e5bfc1
yeung <88296511+yeeun426@users.noreply.github.com>
2024-02-06 20:35:04 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,20 @@
+function solution(n, lost, reserve) {
+    var answer = 0;
+    const students = Array(n).fill(1);
+    
+    lost.forEach(st => students[st-1]--);
+    reserve.forEach(st => students[st-1]++);
+    
+    for(let i = 0 ; i < students.length ; i++) {
+        if(students[i] !== 0) continue;
+        if(students[i-1] > 1) {
+            students[i]++;
+            students[i-1]--;
+        } else if(students[i+1] > 1) {
+            students[i]++;
+            students[i+1]--;
+        }
+    }
+    students.forEach(st => {if(st > 0) answer++});
+    return answer;
+}

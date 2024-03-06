89e1b0867dad4d97e2adc8803d33b8edf8fe39fd
yeung <88296511+yeeun426@users.noreply.github.com>
2024-03-06 16:17:44 +0900

    [level 3] Title: 등굣길, Time: 2.43 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,18 +1,22 @@
 function solution(m, n, puddles) {
-    const pool = [];
-    for(let i = 1; i <= n ; i++) pool.push(Array(m).fill(0));
-    pool[0][0] = 1;
-    puddles.forEach(e => {
-        let [x, y] = e;
-        pool[y-1][x-1] = -1;
-    });
-    pool.forEach((arr, y) => {
-        arr.forEach((e, x) => {
-            if(e !== -1){let n = e + (x > 0 && pool[y][x-1] !== -1 ? pool[y][x-1] : 0) +
-                (y > 0 && pool[y-1][x] !== -1 ? pool[y-1][x] : 0);
-                        n = n % 1000000007;
-                        pool[y][x] = n;}
-        })
-    });
-   return pool[n-1][m-1] % 1000000007;
+    const dp = Array.from(Array(n+1), () => Array(m+1).fill(1)); 
+
+    puddles.forEach(([x, y]) => {
+        dp[y][x] = 0;
+        if(x === 1) {
+            for(let i = y+1; i <= n; i++) dp[i][x] = 0;
+        }
+        if(y === 1) {
+            for(let i = x+1; i <= m; i++) dp[y][i] = 0;
+        }
+    })
+    
+    for(let x = 1 ; x <= m ; x++){
+        for(let y = 1 ; y <= n ; y++){
+            if(x - 1 < 1 || y - 1 < 1) continue;
+            if(dp[y][x] === 0) continue;
+            dp[y][x] = (dp[y][x-1] + dp[y-1][x]) % 1000000007;
+        }
+    }
+    return dp[n][m];
 }

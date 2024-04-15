35331d9554af0db46df1e92f29209f4edfe5d657
yeung <88296511+yeeun426@users.noreply.github.com>
2024-04-15 23:15:49 +0900

    [Silver I] Title: IOIOI, Time: 236 ms, Memory: 15468 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -1,16 +1,17 @@
 let [N, M, S] = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-let strP = "";
 let answer = 0;
 
-for (let i = 0; i < +N ; i++) {
-    strP += "IO";
-    if(i === N-1) strP += ("I");
+let [left, right] = [0, 0];
+while (right < M) {
+    if (S.slice(right, right + 3) === 'IOI') {
+        right += 2;
+        if (right - left === 2 * N) {
+            answer += 1;
+            left += 2;
+        }
+    } else {
+        left = right = right + 1;
+    }
 }
 
-for (let i = 0 ; i < +M ; i++) {
-    const word = S.substring(i, strP.length + i);
-    if(word.length < strP.length) break;
-    if(word === strP) answer++;
-}
-
-console.log(answer)
+console.log(answer);

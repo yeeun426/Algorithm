57824e585ef6db199d8f786aa5f05f6c26d79ef3
yeeun426 <thsudkcla7@naver.com>
2024-05-20 23:45:06 +0900

    feat: boj

---
+++ BOJ/9613.js
@@ -0,0 +1,24 @@
+let input = require('fs').readFileSync('예제.txt').toString().trim().split("\n");
+const t = input[0];
+
+function getGCD(a, b){
+    while(b != 0){
+        tmp = a % b;
+        a = b;
+        b = tmp;
+    }
+    return a;
+}
+  
+for (let i = 1 ; i <= t ; i++ ) {
+    const testCase = input[i].split(" ").map(Number);
+    const n = testCase[0];
+    let sum = 0;
+
+    for (let j = 1 ; j < n ; j++) {
+        for (let k = j + 1 ; k < n + 1 ; k++) {
+			sum += getGCD(testCase[j], testCase[k])
+        }
+    }
+    console.log(sum);
+}

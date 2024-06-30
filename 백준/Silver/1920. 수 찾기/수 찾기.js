1ef7dc49597541fcc68c8d0d87575fa4909b503d
yeung <88296511+yeeun426@users.noreply.github.com>
2024-07-01 00:31:12 +0900

    [Silver IV] Title: 수 찾기, Time: 324 ms, Memory: 41440 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,30 +1,34 @@
-let input = require("fs")
+let [N, A, M, M_arr] = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .split("\n");
-const A = input[1].split(' ').map(Number);
-const B = input[3].split(' ').map(Number);
-let answer = [];
-A.sort((a,b) => a - b);
-B.forEach(v => {
-    let start = 0;
-    let end = A.length - 1;
-    let res = false;
-    while (start <= end) {
-        let mid = parseInt((start + end) / 2);
-        if (v < A[mid]) { 
-            end = mid - 1; 
-        } else if (v > A[mid]) { 
-            start = mid + 1;
-        } else {
-            res = true;
-            break;
-        }
-    }
-    if (res === true) {
-        answer.push(1);
+
+[N, M] = [Number(N), Number(M)];
+A = A.split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+M_arr = M_arr.split(" ").map(Number);
+const answer = [];
+
+M_arr.forEach((el) => {
+  let start = 0;
+  let end = N - 1;
+  let isNumberInArr = false;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (el > A[mid]) {
+      start = mid + 1;
+    } else if (el < A[mid]) {
+      end = mid - 1;
     } else {
-        answer.push(0);
+      isNumberInArr = true;
+      break;
     }
-})
-console.log(answer.join('\n'));
+  }
+  if (isNumberInArr) answer.push(1);
+  else answer.push(0);
+});
+
+console.log(answer.join("\n"));

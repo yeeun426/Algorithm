9db40380e63a8a61b5ea30dd7578f5bf10197247
yeung <88296511+yeeun426@users.noreply.github.com>
2024-07-03 23:37:24 +0900

    [Silver III] Title: N과 M (5), Time: 240 ms, Memory: 23164 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -0,0 +1,26 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+const [N, M] = input[0].split(" ").map(Number);
+const N_arr = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+const s = [];
+const answer = [];
+
+function dfs() {
+  if (s.length === M) {
+    answer.push(s.join(" ") + "\n");
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (!s.includes(N_arr[i])) {
+      s.push(N_arr[i]);
+      dfs(N_arr[i + 1]);
+      s.pop();
+    }
+  }
+}
+
+dfs();
+console.log(answer.join(""));

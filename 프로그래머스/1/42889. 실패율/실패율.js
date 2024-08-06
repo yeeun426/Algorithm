d0cf2756c776477433dbecd7d03f93bf5e279d6b
yeung <88296511+yeeun426@users.noreply.github.com>
2024-08-06 23:52:46 +0900

    [level 1] Title: 실패율, Time: 8.37 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,26 @@
+function solution(N, stages) {
+  var answer = [];
+  let curPlayer = stages.length;
+  const curStage = {};
+
+  for (let i = 1; i <= N; i++) {
+    curStage[i] = 0;
+  }
+
+  stages.forEach((stage) => {
+    if (curStage[stage] >= 0) {
+      curStage[stage]++;
+    }
+  });
+
+  for (let key in curStage) {
+    curPlayer -= curStage[key];
+    curStage[key] /= curPlayer + curStage[key];
+  }
+
+  let failArr = Object.entries(curStage);
+  failArr.sort((a, b) => b[1] - a[1]);
+  answer = failArr.map(([key, value]) => Number(key));
+
+  return answer;
+}

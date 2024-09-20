9e0baa171f248c14faac31f256695be9a9173dc1
yeung <88296511+yeeun426@users.noreply.github.com>
2024-09-20 23:15:50 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2604.43 ms, Memory: 57.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,15 @@
+function solution(elements) {
+  const elSet = new Set();
+
+  for (let length = 1; length <= elements.length; length++) {
+    for (let i = 0; i <= elements.length; i++) {
+      let sum = 0;
+      for (let j = i; j < length + i; j++) {
+        sum += elements[j % elements.length];
+      }
+      elSet.add(sum);
+    }
+  }
+
+  return elSet.size;
+}

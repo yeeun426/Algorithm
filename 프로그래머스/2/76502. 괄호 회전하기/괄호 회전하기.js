fb4a0389c99b7132346c143df1c9b6ea363d0342
yeung <88296511+yeeun426@users.noreply.github.com>
2024-09-24 17:07:44 +0900

    [level 2] Title: 괄호 회전하기, Time: 43.73 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,29 @@
+// 괄호 -> 스택 사용
+function solution(s) {
+  var answer = 0;
+  let isCorrect = true;
+  const stack = [];
+
+  if (s.length % 2 === 1) return 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let string = s.slice(i) + s.slice(0, i);
+    isCorrect = true;
+
+    for (n of string) {
+      if (n === "[" || n === "{" || n === "(") {
+        stack.push(n);
+      } else {
+        let topStr = stack.pop();
+        if (topStr === "[" && n === "]") continue;
+        if (topStr === "{" && n === "}") continue;
+        if (topStr === "(" && n === ")") continue;
+
+        isCorrect = false;
+        break;
+      }
+    }
+    if (isCorrect) answer++;
+  }
+  return answer;
+}

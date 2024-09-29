f6fb57308e925c25f4695e31ef16b7a86d4e8021
yeung <88296511+yeeun426@users.noreply.github.com>
2024-09-29 23:39:28 +0900

    [level 2] Title: [1차] 캐시, Time: 52.63 ms, Memory: 51.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -0,0 +1,25 @@
+function solution(cacheSize, cities) {
+  var answer = 0;
+  const stack = [];
+  cities = cities.join(",").toLowerCase().split(",");
+
+  for (let i = 0; i < cities.length; i++) {
+    if (cacheSize === 0) return cities.length * 5;
+    let index = stack.indexOf(cities[i]);
+
+    if (stack.length < cacheSize && index < 0) {
+      stack.push(cities[i]);
+      answer += 5;
+    } else {
+      if (index >= 0) {
+        answer += 1;
+        stack.splice(index, 1);
+      } else {
+        answer += 5;
+        stack.shift();
+      }
+      stack.push(cities[i]);
+    }
+  }
+  return answer;
+}

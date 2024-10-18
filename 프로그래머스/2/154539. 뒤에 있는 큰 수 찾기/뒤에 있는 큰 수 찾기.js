dec186c59820031298fda52544614aff220df9b4
yeung <88296511+yeeun426@users.noreply.github.com>
2024-10-18 21:45:18 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 245.18 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  var answer = new Array(numbers.length).fill(-1);
+  const stack = [];
+    
+  for (let i = 0; i < numbers.length; i++) {
+    while (stack && numbers[stack.at(-1)] < numbers[i]) {
+      answer[stack.pop()] = numbers[i];
+    }
+    stack.push(i);
+  }
+  return answer;
+}

14044bccfeb077b7cb336febc8391bd070b2b6f3
yeung <88296511+yeeun426@users.noreply.github.com>
2024-12-18 02:45:07 +0900

    [level 2] Title: 숫자 변환하기, Time: 56.86 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,12 @@
+function solution(x, y, n) {
+  let d = [...Array(y + 1).fill(Infinity)];
+  d[x] = 0;
+
+  for (let i = x; i <= y; i++) {
+    if (i - n >= x) d[i] = Math.min(d[i], d[i - n] + 1);
+    if (i % 2 === 0 && i / 2 >= x) d[i] = Math.min(d[i], d[i / 2] + 1);
+    if (i % 3 === 0 && i / 3 >= x) d[i] = Math.min(d[i], d[i / 3] + 1);
+  }
+
+  return d[y] === Infinity ? -1 : d[y];
+}

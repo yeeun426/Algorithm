dc8562bbf42817ff0d35c7f41e1d3d9b579ea9b0
yeung <88296511+yeeun426@users.noreply.github.com>
2025-01-09 23:38:09 +0900

    [level 2] Title: [3차] 압축, Time: 0.69 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,26 +1,33 @@
 function solution(msg) {
-  var answer = [];
-  const index = [];
-
-  for (let i = 0; i < 26; i++) {
-    index.push(String.fromCharCode(i + 65));
-  }
-
-  for (let i = 0; i < msg.length; i++) {
-    let [w, c] = [msg[i], msg[i + 1]];
-
-    while (index.includes(w)) {
-      let message = w + c;
-      if (index.includes(message)) {
-        w += c;
-        c = msg[++i + 1];
-      } else {
-        answer.push(index.indexOf(w) + 1);
-        index.push(w + c);
-        break;
-      }
+    let answer = [];
+    let dic = {};
+    
+    // 초기 사전 생성
+    for (let i = 0; i < 26; i++) {
+        dic[String.fromCharCode(i + 65)] = i + 1;
     }
-  }
-
-  return answer;
+    
+    let currentWord = '';
+    let nextIndex = 27; // A-Z를 넘어서는 색인
+    
+    for (let i = 0; i < msg.length; i++) {
+        currentWord += msg[i];
+        
+        // 사전에 없는 경우
+        if (!(currentWord in dic)) {
+            // 이전 단어의 색인을 결과에 추가
+            answer.push(dic[currentWord.slice(0, -1)]);
+            // 새 단어를 사전에 추가
+            dic[currentWord] = nextIndex++;
+            // currentWord 초기화
+            currentWord = msg[i];
+        }
+    }
+    
+    // 마지막 단어 처리
+    if (currentWord) {
+        answer.push(dic[currentWord]);
+    }
+    
+    return answer;
 }

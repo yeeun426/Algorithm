4af2ae82df122b53d4278fed2386a85536962845
yeung <88296511+yeeun426@users.noreply.github.com>
2025-01-15 23:50:13 +0900

    [level 3] Title: 단속카메라, Time: 2.92 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,11 +1,9 @@
 function solution(routes) {
     routes.sort((a, b) => a[1] - b[1]);
-    console.log(routes);
     var answer = 1;
     let [start, out] = routes.shift();
 
     while(routes.length > 0) {
-        
         if(out >= routes[0][0] && out <= routes[0][1]) {
             routes.shift();
         } else {

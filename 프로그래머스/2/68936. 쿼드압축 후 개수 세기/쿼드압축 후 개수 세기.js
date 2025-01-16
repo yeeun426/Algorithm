36b0105b0f20e00c70ec209776ae42f0262b7044
yeung <88296511+yeeun426@users.noreply.github.com>
2025-01-16 23:54:15 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 29.26 ms, Memory: 40.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -0,0 +1,30 @@
+function solution(arr) {
+  var answer = [0, 0];
+
+  function divide(row, col, len) {
+    let dividable = true;
+
+    for (let i = row; i < row + len; i++) {
+      for (let j = col; j < col + len; j++) {
+        if (arr[row][col] !== arr[i][j]) {
+          dividable = false;
+        }
+      }
+    }
+
+    let half = Math.floor(len / 2);
+
+    if (!dividable) {
+      divide(row, col, half); // 2 quad
+      divide(row + half, col, half); // 1 quad
+      divide(row, col + half, half); // 3 quad
+      divide(row + half, col + half, half); // 4 quad
+    } else {
+      if (arr[row][col] === 1) answer[1]++;
+      else answer[0]++;
+    }
+  }
+
+  divide(0, 0, arr.length);
+  return answer;
+}

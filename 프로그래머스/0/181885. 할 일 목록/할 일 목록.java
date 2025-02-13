ac289180969c501baf07beab33089232111e10bc
yeung <88296511+yeeun426@users.noreply.github.com>
2025-02-13 15:46:06 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<String> solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(int i = 0 ; i < todo_list.length ; i++) {
+            if(!finished[i]) answer.add(todo_list[i]);
+        }
+        return answer;
+    }
+}

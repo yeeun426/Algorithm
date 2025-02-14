aca7f29a13b428d65d300054bf0416201734fd00
yeung <88296511+yeeun426@users.noreply.github.com>
2025-02-14 15:26:59 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if(a % 2 != 0 && b % 2 != 0) {
+            return a*a + b*b;
+        } else if (a % 2 == 0 && b % 2 == 0) {
+            return Math.abs(a-b);
+        } else {
+            return 2 * (a+b);
+        }
+    }
+}

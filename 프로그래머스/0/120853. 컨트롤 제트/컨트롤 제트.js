781b57144f0ab84482807739d7e14c0af8b7ff98
yeung <88296511+yeeun426@users.noreply.github.com>
2025-02-18 11:24:04 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    var answer = 0;
+    const arr = s.split(" ");
+    
+    for(let i = 0 ; i < arr.length ; i++) {
+        if(arr[i] == "Z") {
+            answer -= Number(arr[i-1]);
+        } else {
+            answer += Number(arr[i]);
+        }
+    }
+    
+    return answer;
+}

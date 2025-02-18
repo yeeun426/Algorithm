052bb29bb30355fd7955c32b82fa9b7d54cf4036
yeung <88296511+yeeun426@users.noreply.github.com>
2025-02-18 14:59:52 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] str_list) {
+    String[] str=new String[0];
+        for (int i = 0; i < str_list.length; i++) {
+
+            if ("l".equals(str_list[i])) {
+                return Arrays.copyOfRange(str_list, 0, i);
+            } else if ("r".equals(str_list[i])) {
+                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
+            }
+        }
+
+        return str;
+    }
+}

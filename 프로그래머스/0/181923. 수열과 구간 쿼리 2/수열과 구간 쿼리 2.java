9d408aec99d40001c99e2d2fe395d80c7ba25ec3
yeung <88296511+yeeun426@users.noreply.github.com>
2025-02-18 14:30:30 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 3.26 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -7,17 +7,16 @@ class Solution {
         for (int i = 0; i < queries.length; i++) {
             int s = queries[i][0], e = queries[i][1], k = queries[i][2];
             
-            int minValue = -1; 
+            int min = -1; 
             for (int j = s; j <= e; j++) {
                 if (arr[j] > k) {
-                    if (minValue == -1 || arr[j] < minValue) {
-                        minValue = arr[j];
+                    if (min == -1 || arr[j] < min) {
+                        min = arr[j];
                     }
                 }
             }
-            answer[i] = minValue;
+            answer[i] = min;
         }
-        
         return answer;
     }
 }

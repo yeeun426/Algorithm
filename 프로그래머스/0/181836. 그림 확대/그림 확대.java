75c23dae9aa9db25536705dd423dfb141342fa3c
yeung <88296511+yeeun426@users.noreply.github.com>
2025-02-18 14:47:13 +0900

    [level 0] Title: 그림 확대, Time: 0.34 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,19 +1,19 @@
 class Solution {
     public String[] solution(String[] picture, int k) {
-        int n = picture.length;
-        int m = picture[0].length();
+        int rows = picture.length;
+        int cols = picture[0].length();
+        String[] answer = new String[rows * k]; 
         
-        String[] answer = new String[n * k]; 
-        for (int i = 0; i < n; i++) {
-            StringBuilder enlargedRow = new StringBuilder();
-            for (int j = 0; j < m; j++) {
+        for (int i = 0; i < rows; i++) {
+            StringBuilder bigRow = new StringBuilder();
+            for (int j = 0; j < cols; j++) {
                 char c = picture[i].charAt(j);
                 for (int l = 0; l < k; l++) {
-                    enlargedRow.append(c);
+                    bigRow.append(c);
                 }
             }
             for (int p = 0; p < k; p++) {
-                answer[i * k + p] = enlargedRow.toString();
+                answer[i * k + p] = bigRow.toString();
             }
         }
         return answer;

0afd7f55367595fd016f30b86ac4f7b650d77d8c
yeung <88296511+yeeun426@users.noreply.github.com>
2025-02-24 22:35:46 +0900

    [level 2] Title: [3차] n진수 게임, Time: 12.11 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,17 +1,18 @@
 function solution(n, t, m, p) {
-  var answer = "";
-  let i = 0;
-  let tubeArr = "";
+    var answer = '';
+    let cnt = 0;
+    let idx = p;
+    let allStr = "";
+    
+    while (cnt <= m * t) {
+        allStr += cnt.toString(n).toUpperCase();
+        cnt++;    
+        if(allStr.length > m * t) break;
+    }
 
-  while (i <= m * t) {
-    let formation = i.toString(n).toUpperCase();
-    tubeArr += formation;
-    i++;
-  }
-
-  for (let i = 0; i < t; i++) {
-    answer += tubeArr[p - 1];
-    p += m;
-  }
-  return answer;
+    for(let i = 0 ; i < t ; i++) {
+        answer += allStr[idx-1];
+        idx += m;
+    }
+    return answer;
 }

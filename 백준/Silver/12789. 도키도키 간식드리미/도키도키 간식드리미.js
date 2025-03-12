05e7fff8930417f7f686cbccb3b9aac6cec9acce
yeung <88296511+yeeun426@users.noreply.github.com>
2025-03-12 13:49:19 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 92 ms, Memory: 9400 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.js
@@ -0,0 +1,29 @@
+let [N, ...input] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+input = input.toString().split(" ").map(Number);
+const students = [...input];
+const onePeople = [];
+let order = 1;
+
+while (students.length > 0 || onePeople.length > 0) {
+  if (students.length == 0 && onePeople[onePeople.length - 1] > order) {
+    return console.log("Sad");
+  }
+
+  if (onePeople[onePeople.length - 1] == order) {
+    order++;
+    onePeople.pop();
+    continue;
+  } else if (students[0] == order) {
+    order++;
+    students.shift();
+    continue;
+  }
+  onePeople.push(students.shift());
+}
+
+console.log("Nice");

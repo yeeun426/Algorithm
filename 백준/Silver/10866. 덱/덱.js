9cabcd4d520671950b759f11b17f90d5b16b32a8
yeung <88296511+yeeun426@users.noreply.github.com>
2025-03-28 17:43:53 +0900

    [Silver IV] Title: 덱, Time: 120 ms, Memory: 13152 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.js
+++ 백준/Silver/10866. 덱/덱.js
@@ -1,49 +1,44 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
-let N = +input.shift();
+let [N, ...inputs] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
-const result = [];
 const deque = [];
+const answer = [];
 
-for(let i = 0 ; i < N ; i++) {
-    const command = input[i].trim().split(" ")[0];
-    const frontDQ = []
-    
-    switch(command) {
-        case 'push_front' :
-            deque.unshift(+input[i].split(" ")[1])
-            break;
+for (let input of inputs) {
+  let [command, num] = input.split(" ");
+  num = Number(num);
 
-        case 'push_back' :
-            deque.push(+input[i].split(" ")[1]);
-            break;
-
-        case 'pop_front' :
-            result.push(deque.shift() || -1);
-            break;
-
-        case 'pop_back' :
-            result.push(deque.pop() || -1);            
-            break;
-
-        case 'size' :   
-            result.push(deque.length);
-            break;
-
-        case 'empty' :
-            result.push(deque.length ? 0 : 1);
-            break;
-
-        case 'front' :
-            result.push(deque[0] || -1);
-            break;
-
-        case 'back' :
-            result.push(deque[deque.length-1] || -1);
-            break;
-
-        default :
-            break;
-   }
+  switch (command) {
+    case "push_front":
+      deque.unshift(num);
+      break;
+    case "push_back":
+      deque.push(+num);
+      break;
+    case "pop_front":
+      answer.push(deque.length ? deque.shift() : -1);
+      break;
+    case "pop_back":
+      answer.push(deque.length ? deque.pop() : -1);
+      break;
+    case "size":
+      answer.push(deque.length);
+      break;
+    case "empty":
+      answer.push(deque.length == 0 ? 1 : 0);
+      break;
+    case "front":
+      answer.push(deque.length ? deque[0] : -1);
+      break;
+    case "back":
+      answer.push(deque.length ? deque[deque.length - 1] : -1);
+      break;
+    default:
+      break;
+  }
 }
 
-console.log(result.join("\n"));
+console.log(answer.join("\n"));

ef258cd5ac4be6d239b5d79b66b10fe28b25985a
yeeun426 <thsudkcla7@naver.com>
2025-04-19 23:50:02 +0900

    feat: algorithm-study-week7

---
+++ BOJ/week7/10026.js
@@ -0,0 +1,120 @@
+let [N, ...board] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+board = board.map((color) => color.split(""));
+const visited = Array.from({ length: +N }, () => Array(+N).fill(0)); // 적록색약 X
+
+const rgBoard = board.map((row) =>
+  row.map((color) => (color === "R" ? "G" : color)),
+);
+const rgVisited = Array.from({ length: +N }, () => Array(+N).fill(0)); // 적록색약 O
+
+const answer = [];
+let count = 0;
+
+const dx = [1, 0, -1, 0];
+const dy = [0, 1, 0, -1];
+
+const bfs = (startX, startY, vis, board) => {
+  const queue = [[startX, startY]];
+  vis[startX][startY] = 1;
+  count++;
+
+  while (queue.length > 0) {
+    const [x, y] = queue.shift();
+    let color = board[x][y];
+
+    for (let dir = 0; dir < 4; dir++) {
+      const [nx, ny] = [x + dx[dir], y + dy[dir]];
+      if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+      if (vis[nx][ny] == 1 || board[nx][ny] !== color) continue;
+
+      vis[nx][ny] = 1;
+      queue.push([nx, ny]);
+    }
+  }
+};
+
+// 적록 색약 아닌 경우
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    if (visited[i][j] === 0) bfs(i, j, visited, board);
+  }
+}
+
+answer.push(count);
+count = 0;
+
+// 적록 색약인 경우
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    if (rgVisited[i][j] === 0) bfs(i, j, rgVisited, rgBoard);
+  }
+}
+
+answer.push(count);
+console.log(answer.join(" "));
+
+// let [N, ...board] = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .trim()
+//   .split("\n");
+
+// board = board.map((color) => color.split(""));
+// const visited = Array.from({ length: +N }, () => Array(+N).fill(0)); // 적록색약 X
+// const rgVisited = Array.from({ length: +N }, () => Array(+N).fill(0)); // 적록색약 O
+
+// const answer = [];
+// let count = 0;
+
+// const dx = [1, 0, -1, 0];
+// const dy = [0, 1, 0, -1];
+
+// const bfs = (startX, startY, vis, isRG) => {
+//   const queue = [[startX, startY]];
+//   vis[startX][startY] = 1;
+//   count++;
+
+//   while (queue.length > 0) {
+//     const [x, y] = queue.shift();
+//     let color = board[x][y];
+//     if (isRG && color == "R") color = "G";
+
+//     for (let dir = 0; dir < 4; dir++) {
+//       const [nx, ny] = [x + dx[dir], y + dy[dir]];
+//       if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+//       if (!isRG && (vis[nx][ny] == 1 || board[nx][ny] !== color)) continue;
+
+//       let boardColor = board[nx][ny];
+//       if (isRG && board[nx][ny] == "R") boardColor = "G";
+//       if (isRG && (vis[nx][ny] == 1 || boardColor !== color)) continue;
+
+//       vis[nx][ny] = 1;
+//       queue.push([nx, ny]);
+//     }
+//   }
+// };
+
+// // 적록 색약 아닌 경우
+// for (let i = 0; i < N; i++) {
+//   for (let j = 0; j < N; j++) {
+//     if (visited[i][j] === 0) bfs(i, j, visited, false);
+//   }
+// }
+
+// answer.push(count);
+// count = 0;
+
+// // 적록 색약인 경우
+// for (let i = 0; i < N; i++) {
+//   for (let j = 0; j < N; j++) {
+//     if (rgVisited[i][j] === 0) bfs(i, j, rgVisited, true);
+//   }
+// }
+
+// answer.push(count);
+// console.log(answer.join(" "));
---
+++ BOJ/week7/1012.js
@@ -0,0 +1,56 @@
+let [T, ...input] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let worm = 0;
+let idx = 0;
+const dx = [1, 0, -1, 0];
+const dy = [0, 1, 0, -1];
+
+// N: 세로 (행 개수) → 보통 x축으로 취급 (위아래 방향)
+// M: 가로 (열 개수) → 보통 y축으로 취급 (좌우 방향)
+
+while (idx < input.length) {
+  let [M, N, K] = input[idx].split(" ").map(Number);
+  const field = Array.from({ length: N }, () => Array(M).fill(0)); // 배추밭
+  const visited = Array.from({ length: N }, () => Array(M).fill(0)); // 거리를 저장할 변수
+  for (let j = idx + 1; j < K + idx + 1; j++) {
+    const [y, x] = input[j].split(" ").map(Number);
+    field[x][y] = 1;
+  }
+  for (let x = 0; x < N; x++) {
+    for (let y = 0; y < M; y++) {
+      if (field[x][y] == 1 && visited[x][y] == 0) {
+        // 배추있고,방문 x
+        bfs(x, y, visited, field);
+      }
+    }
+  }
+  idx += K + 1;
+  console.log(worm);
+  worm = 0;
+}
+
+function bfs(startX, startY, visited, field) {
+  const queue = [[startX, startY]];
+  visited[startX][startY] = 1;
+  worm++;
+
+  while (queue.length > 0) {
+    const [x, y] = queue.shift();
+
+    for (let dir = 0; dir < 4; dir++) {
+      const [nx, ny] = [x + dx[dir], y + dy[dir]];
+
+      if (nx < 0 || nx >= field.length || ny < 0 || ny >= field[0].length)
+        continue;
+
+      if (field[nx][ny] == 0 || visited[nx][ny] == 1) continue;
+
+      queue.push([nx, ny]);
+      visited[nx][ny] = 1;
+    }
+  }
+}

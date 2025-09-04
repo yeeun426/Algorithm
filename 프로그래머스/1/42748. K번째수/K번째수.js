4cb1720b943f8b0f43308dfa1a55ba531f4fc592
yeung <88296511+yeeun426@users.noreply.github.com>
2025-09-04 22:21:04 +0900

    [level 1] Title: K번째수, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,15 @@
+function HandleSliceArr(array,i,j) {
+    const sliceArr = array.slice(i-1,j);
+    return sliceArr.sort((a,b) => a-b);
+}
 function solution(array, commands) {
     var answer = [];
     
-    for(let a = 0 ; a < commands.length ; a++) {
-        const [i, j, k] = commands[a];
-        const newArr = array.slice(i-1, j);
-        newArr.sort((a,b) => a-b);
-        answer.push(newArr[k-1]);
+    for(let p = 0 ; p < commands.length ; p++) {
+        const [i,j,k] = commands[p]
+        const sliceArr = HandleSliceArr(array, i,j);
+        answer.push(sliceArr[k-1])
     }
+    
     return answer;
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?gad_source=1&gclid=CjwKCAjwwLO_BhB2EiwAx2e-3xtGwUv3wN7A5K79OdzUY45ovHw-DSdMUQUGJ3WbnO7n2UlCtLcBHhoC0Y0QAvD_BwE) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.12 ms
+메모리: 33.4 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 02일 22:02:10
+2025년 09월 04일 22:21:00
 
 ### 문제 설명
 

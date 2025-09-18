f901a391b61b0b5bfdd9a7cd06bd0308777bbbe4
yeung <88296511+yeeun426@users.noreply.github.com>
2025-09-19 00:14:47 +0900

    [level 2] Title: [1차] 캐시, Time: 484.62 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,24 +1,24 @@
 function solution(cacheSize, cities) {
   var answer = 0;
-  const stack = [];
-  cities = cities.join(",").toLowerCase().split(",");
+  const cacheMap = new Map();
 
+  if (cacheSize === 0) return cities.length * 5;
   for (let i = 0; i < cities.length; i++) {
-    if (cacheSize === 0) return cities.length * 5;
-    let index = stack.indexOf(cities[i]);
-
-    if (stack.length < cacheSize && index < 0) {
-      stack.push(cities[i]);
-      answer += 5;
+    const city = cities[i].toLowerCase();
+    const idx = cacheMap.get(city);
+    if (idx !== undefined) {
+      cacheMap.delete(city);
+      cacheMap.set(city, i);
+      answer += 1;
     } else {
-      if (index >= 0) {
-        answer += 1;
-        stack.splice(index, 1);
-      } else {
-        answer += 5;
-        stack.shift();
+      if (cacheMap.size >= cacheSize && cacheSize > 0) {
+        const oldestCity = [...cacheMap.entries()].reduce((a, b) =>
+          a[1] < b[1] ? a : b,
+        )[0];
+        cacheMap.delete(oldestCity);
       }
-      stack.push(cities[i]);
+      cacheMap.set(city, i);
+      answer += 5;
     }
   }
   return answer;

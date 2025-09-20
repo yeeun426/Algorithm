405106d28da931e6ab55a1e1d5732fb19ae5b737
yeung <88296511+yeeun426@users.noreply.github.com>
2025-09-20 19:06:59 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.98 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,34 +1,49 @@
 function solution(str1, str2) {
   var answer = 0;
+  let inter = 0;
+  let union = 0;
 
-  const strArr1 = [];
-  const strArr2 = [];
-
+  str1 = str1.toLowerCase();
+  const str1Arr = [];
   for (let i = 0; i < str1.length - 1; i++) {
-    const string1 = str1.toLowerCase().slice(i, i + 2);
-    if (/^[a-z]{2}$/.test(string1)) strArr1.push(string1);
-  }
-  for (let i = 0; i < str2.length - 1; i++) {
-    const string2 = str2.toLowerCase().slice(i, i + 2);
-    if (/^[a-z]{2}$/.test(string2)) strArr2.push(string2);
-  }
-
-  let intersection = 0;
-  let union = strArr1.length + strArr2.length;
-
-  if (strArr1.length === 0 && strArr2.length === 0) return 65536;
-
-  for (let i = 0; i < strArr1.length; i++) {
-    for (let j = 0; j < strArr2.length; j++) {
-      if (strArr1[i] === strArr2[j]) {
-        intersection++;
-        strArr2.splice(j, 1);
-        j--;
-        break;
-      }
+    const pair = str1[i] + str1[i + 1];
+    if (/^[a-z]{2}$/.test(pair)) {
+      str1Arr.push(pair);
     }
   }
-    
-  answer = intersection / (union - intersection);
-  return parseInt(answer * 65536);
+
+  str2 = str2.toLowerCase();
+  const str2Arr = [];
+  for (let i = 0; i < str2.length - 1; i++) {
+    const pair = str2[i] + str2[i + 1];
+    if (/^[a-z]{2}$/.test(pair)) {
+      str2Arr.push(pair);
+    }
+  }
+
+  const map1 = new Map();
+  const map2 = new Map();
+
+  str1Arr.forEach((v) => {
+    map1.set(v, (map1.get(v) || 0) + 1);
+  });
+  str2Arr.forEach((v) => {
+    map2.set(v, (map2.get(v) || 0) + 1);
+  });
+
+  for (let [str, cnt] of map1) {
+    if (map2.has(str)) {
+      inter += Math.min(cnt, map2.get(str));
+      union += Math.max(cnt, map2.get(str));
+      map2.delete(str);
+    } else {
+      union += cnt;
+    }
+  }
+
+  for (let v of map2.values()) union += v;
+  
+  answer = union === 0 ? 65536 : Math.floor((inter / union) * 65536);
+
+  if (str1) return answer;
 }

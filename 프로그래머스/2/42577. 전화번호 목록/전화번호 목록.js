fcec8bd19eee31b00d571baf2080a1dff74779c8
yeung <88296511+yeeun426@users.noreply.github.com>
2025-10-21 00:19:30 +0900

    [level 2] Title: 전화번호 목록, Time: 259.95 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,23 +1,12 @@
 function solution(phone_book) {
-    var answer = true;
-    const phoneMap = new Map();
+  var answer = true;
+  const phoneBookSet = new Set(phone_book);
 
-    phone_book.sort();
-    
-    for(let i = 0 ; i < phone_book.length-1 ; i++) {
-        let shortNum = "";
-        let prefix = "";
-        
-        if(phone_book[i].length > phone_book[i+1].length)  {
-            shortNum = phone_book[i+1]
-            prefix = phone_book[i].slice(0, phone_book[i+1].length);
-        } else {
-            shortNum = phone_book[i]
-            prefix = phone_book[i+1].slice(0, phone_book[i].length);
-        }
-        
-        if(prefix === shortNum) answer = false;
+  for (let phone of phone_book) {
+    for (let i = 1; i < phone.length; i++) {
+      const prefix = phone.slice(0, i);
+      if (phoneBookSet.has(prefix)) return false;
     }
-    
-    return answer;
+  }
+  return answer;
 }

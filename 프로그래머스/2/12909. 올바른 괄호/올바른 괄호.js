c5adc118cbfa507df29977997043cd25b17b01d7
yeung <88296511+yeeun426@users.noreply.github.com>
2025-10-22 01:48:31 +0900

    [level 2] Title: 올바른 괄호, Time: 32.35 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,31 +1,16 @@
-function solution(s) {
-  const strArr = s.split("");
-  const stack = [];
-
-  for (let i = 0; i < strArr.length; i++) {
-    if (strArr[i] == "(") {
-      stack.push("(");
-    } else if (strArr[i] == ")" && stack.length > 0) {
-      stack.pop();
-    } else {
-      stack.push(strArr[i]);
-      break;
+function solution(s){
+    const array = s.split("");
+    const stack = [];
+    
+    if(array[0] === ")") return false;
+    
+    for(let str of array) {
+        if(stack[stack.length - 1] === "(" && str === ")") {
+            stack.pop();
+        } else {
+            stack.push(str);
+        }
     }
-  }
-  return stack.length ? false : true
-}
 
-// function solution(s){
-//     var answer = true;
-//     const stack = [];    
-//     for(let i = 0 ; i < s.length ; i++) {
-//         if(s[0] === ")") {
-//             answer = false;
-//             break;
-//         }
-//         if(s[i] === "(") stack.push(s[i]);
-//         else if(s[i] === ")") stack.pop();
-//     }
-//     if(stack.length) answer = false
-//     return answer;
-// }
+    return stack.length === 0;
+}

de3877d3f148a92f73de5d2f42a76fbfb5d35f78
yeung <88296511+yeeun426@users.noreply.github.com>
2025-10-23 15:39:30 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 5.29 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.3 MB, 시간: 5.42 ms
+메모리: 36.4 MB, 시간: 5.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 10일 23:50:57
+2025년 10월 23일 15:39:27
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,23 +1,90 @@
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  getParentIndex(i) {
+    return Math.floor((i - 1) / 2);
+  }
+  getLeftChildIndex(i) {
+    return i * 2 + 1;
+  }
+  getRightChildIndex(i) {
+    return i * 2 + 2;
+  }
+
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      const parent = this.getParentIndex(index);
+      if (this.heap[parent][1] <= this.heap[index][1]) break; // job[1] 기준 (작업 시간)
+      this.swap(parent, index);
+      index = parent;
+    }
+  }
+
+  pop() {
+    if (this.heap.length === 1) return this.heap.pop();
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return root;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    const length = this.heap.length;
+    while (this.getLeftChildIndex(index) < length) {
+      let smallerChild = this.getLeftChildIndex(index);
+      const right = this.getRightChildIndex(index);
+      if (right < length && this.heap[right][1] < this.heap[smallerChild][1]) {
+        smallerChild = right;
+      }
+      if (this.heap[index][1] <= this.heap[smallerChild][1]) break;
+      this.swap(index, smallerChild);
+      index = smallerChild;
+    }
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+}
+
 function solution(jobs) {
-    var answer = 0;
-    jobs.sort((a, b) => a[0] - b[0]);
-    const queue = [];
-    let time = 0;
-    let i = 0;
-    while(queue.length > 0 || i < jobs.length) {
-        if(jobs.length > i && jobs[i][0] <= time) {
-            queue.push(jobs[i++]);
-            queue.sort((a, b) => a[1] - b[1]);
-            continue;
-        }
-        if(queue.length > 0) {
-            let [ start, work ] = queue.shift();
-            answer = answer + time - start + work;
-            time += work;
-        } else if(queue.length == 0) {
-            time = jobs[i][0];
-        }
+  jobs.sort((a, b) => a[0] - b[0]); // 요청 시점 기준 정렬
+
+  const heap = new MinHeap();
+  let time = 0;
+  let total = 0;
+  let index = 0;
+  const length = jobs.length;
+
+  while (index < length || !heap.isEmpty()) {
+    // 현재 시점 이전에 들어온 작업들을 힙에 넣기
+    while (index < length && jobs[index][0] <= time) {
+      heap.push(jobs[index]);
+      index++;
     }
 
-    return parseInt(answer / jobs.length);
+    if (!heap.isEmpty()) {
+      const [start, duration] = heap.pop();
+      time += duration;
+      total += time - start; // 대기 + 실행 시간
+    } else {
+      // 아직 요청이 들어오지 않은 경우
+      time = jobs[index][0];
+    }
+  }
+
+  return Math.floor(total / length);
 }
